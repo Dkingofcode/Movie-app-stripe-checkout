331405e64079b24b1763a6ef65cb794766120bb2
David <doladepo128@gmail.com>
2023-07-23 23:01:32 +0100

    fixed the row component data rendering

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -3,19 +3,26 @@ import React, { useEffect, useState } from 'react';
 import requests from './Requests';
 import "./Banner.css";
 
+const BASE_URL =  'https://api.themoviedb.org/3'
+
 const Banner = () => {
   const [movie, setMovie] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
-       const request = await axios.get(requests.fetchNetflixOriginals);
+      try { 
+       const request = await axios.get(BASE_URL + requests.fetchNetflixOriginals);
        setMovie(
          request.data.results[
            Math.floor(Math.random() * request.data.results.length - 1)
          ]
        );
        return request; 
+    } catch(error){
+      console.error('Error fetching data');
+      setMovie(null);
     }
+  }
     fetchData();
   }, []);
 
@@ -28,19 +35,19 @@ const Banner = () => {
     return (
     <header className='banner' style={{ backgroundSize: "cover", backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`, backgroundPosition: `center center`,}}>
      <div className='banner__contents'>
-     <h1 className='banner__title'>Movie Name</h1>
+     <h1 className='banner__title'>{movie?.title || movie?.name || movie?.original_name}</h1>
      <div className='banner__buttons'>
       <button className='banner__button'>Play</button>
       <button className='banner__button'>My List</button>   
      </div>
-      <h1 className='banner__description'>{truncate('This is a test description', 150)}</h1>
+      <h1 className='banner__description'>{truncate(movie?.overview, 150)}</h1>
     </div> 
 
     <div className='banner--fadeBottom'>
 
     </div>
      </header>
-  )
+  );
 }
 
 export default Banner;
---
+++ src/Row.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react';
+import axios from './axios';
+import './Row.css';
+//import logo from "./assets/vite.svg";
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import logo from '/boliviainteligente-zhIFruQW6qY-unsplash.jpg'
+
+function Row({ title, fetchUrl, isLargeRow = false })   {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+
+   const base_url = "https://image.tmdb.org/t/p/original/";
+    //https://image.tmdb.org/t/p/original/
+
+   useEffect(() => {
+      async function fetchData() {
+      try{   
+        const request = await axios.get(fetchUrl);
+        setMovies(request.data.results);
+        setLoading(false);
+        return request;
+      }catch (error){
+        console.error('Error fetching data');
+        setLoading(false);
+      }
+    }
+
+      fetchData();
+   }, [fetchUrl]);
+
+     if(loading){
+        return <p>Loading...</p>
+     }
+
+
+
+   return (
+     <div className='row'>
+       <h2 className='row__title'>{title}</h2>
+       <div className='row__posters'>       
+       {/* {console.log(movies)}       */}
+       {movies?.map((movie) => (  
+       movie.poster_path && movie.backdrop_path ?  ( 
+            <img
+            key={movie.id}
+              className="row__poster"
+              src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+              alt={movie.title}
+              />
+          ) : null
+        ))}
+       </div>
+     </div>
+   );
+  }
+
+  export default Row;
